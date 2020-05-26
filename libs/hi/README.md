a41534f4e6a242710cb290ad9e7fa4336476664c
ysyun <ysyun@bistel.com>
2020-05-26 13:39:14 +0900

    chore(schematics): tested custom schematic

---
+++ libs/hi/README.md
@@ -0,0 +1,7 @@
+# hi
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `ng test hi` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/hi/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'hi',
+    preset: '../../jest.config.js',
+    transform: {
+        '^.+\\.[tj]sx?$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+    coverageDirectory: '../../coverage/libs/hi',
+};
