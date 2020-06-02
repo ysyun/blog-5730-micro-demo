6218abf5786e551e1c708c360c8c54468282abdb
ysyun <ysyun@bistel.com>
2020-06-02 16:39:04 +0900

    fix(load-script): update load script using Promise

--- apps/app-container/src/app/monitor.service.ts
+++ apps/app-container/src/app/monitor.service.ts
@@ -1,27 +1,50 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class MonitorLoaderService {
-  loaded = false;
+    loaded = false;
+    private loadedFiles = new Set<string>();
 
-  constructor() { }
+    constructor() {}
 
-  loadMonitorScript(): void {
-    if (this.loaded) {
-      return;
+    loadMonitorScript(): void {
+        if (this.loaded) {
+            return;
+        }
+
+        const script = document.createElement('script');
+        script.src = 'assets/monitor-es5.js';
+        document.body.appendChild(script);
+        this.loaded = true;
     }
 
-    const script = document.createElement('script');
-    script.src = 'assets/monitor-es5.js';
-    document.body.appendChild(script);
-    this.loaded = true;
-  }
+    loadScript(src: string): Promise<void> {
+        if (this.loadedFiles.has(src)) {
+            return Promise.resolve();
+        }
 
-  addMonitorApp(): void {
-    const tile = document.createElement('monitor-app');
-    tile.setAttribute('title', 'Dynamic Load Monitor');
+        return new Promise<void>((resolve, reject) => {
+            const script = document.createElement('script');
+            script.src = src;
 
-    const content = document.getElementById('content');
-    content.appendChild(tile);
-  }
+            script.onload = () => {
+                this.loadedFiles.add(src);
+                resolve();
+            };
+
+            script.onerror = () => {
+                reject();
+            };
+
+            document.body.appendChild(script);
+        });
+    }
+
+    addMonitorApp(): void {
+        const tile = document.createElement('monitor-app');
+        tile.setAttribute('title', 'Dynamic Load Monitor');
+
+        const content = document.getElementById('content');
+        content.appendChild(tile);
+    }
 }
--- apps/app-container/src/app/pages/welcome/welcome.component.ts
+++ apps/app-container/src/app/pages/welcome/welcome.component.ts
@@ -3,15 +3,20 @@ import { Component, OnInit } from '@angular/core';
 // import '../../../assets/monitor-es5.js';
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss'],
+    selector: 'app-welcome',
+    templateUrl: './welcome.component.html',
+    styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
-  constructor(private monitorService: MonitorLoaderService) {}
+    constructor(private monitorService: MonitorLoaderService) {}
 
-  ngOnInit() {
-    this.monitorService.loadMonitorScript();
-    this.monitorService.addMonitorApp();
-  }
+    ngOnInit() {
+        const src = 'assets/monitor-es5.js';
+        this.monitorService.loadScript(src).then(
+            () => {
+                this.monitorService.addMonitorApp();
+            },
+            (error) => console.log('script load failed: ', src)
+        );
+    }
 }
