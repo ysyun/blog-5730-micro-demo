fbf52701fc23a6ff3c03e1cbfd8c0566831781c2
ysyun <ysyun@bistel.com>
2020-06-09 12:09:22 +0900

    feat(storybook): add storybook to test

---
+++ libs/lazyview/src/lib/lazypage/lazypage.component.stories.ts
@@ -0,0 +1,13 @@
+import { LazypageComponent } from './lazypage.component';
+
+export default {
+    title: 'LazypageComponent',
+};
+
+export const primary = () => ({
+    moduleMetadata: {
+        imports: [],
+    },
+    component: LazypageComponent,
+    props: {},
+});
